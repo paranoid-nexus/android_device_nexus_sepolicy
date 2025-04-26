6474e58f34b8feebdd0bb6b9ffacffea9ea7e697
Alexander Koskovich <akoskovich@pm.me>
2025-04-26 03:30:17 -0400

    sepolicy: Add video encode flag opt-out for DeviceAsWebcam
    Change-Id: I3b2d37fe4ab79c136f8cdefdeeb77fa1aed49d02

--- sepolicy.mk
+++ sepolicy.mk
@@ -3,5 +3,8 @@
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/nexus/sepolicy/vendor
 
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    device/neoteric/sepolicy/public
+
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/nexus/sepolicy/private
