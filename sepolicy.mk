5fbbb239766942cafc158b0e9a5b1dfd1bc6544e
CuriousNom <aryanbiswas7585@gmail.com>
2026-04-08 00:33:20 +0530

    sepolicy: Move to legacy-um path
    * We will keep common sepolicy different now.

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,7 +1,6 @@
 # Additional Nexus Sepolicy
-
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/nexus/sepolicy/vendor
+    device/nexus/sepolicy/legacy-um/vendor
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/nexus/sepolicy/private
+    device/nexus/sepolicy/legacy-um/private
