f8706e72efd7e7a1458469d1acfc999955d56516
CuriousNom <aryanbiswas7585@gmail.com>
2026-04-08 11:13:07 +0530

    sepolicy: Initialize Common Nexus Sepolicy
    Change-Id: I98e135b1477c2cc885c4c7e833faa8639d7ef5db

---
+++ sepolicy.mk
@@ -0,0 +1,9 @@
+# Additional Nexus Sepolicy
+#BOARD_VENDOR_SEPOLICY_DIRS += \
+#    device/nexus/sepolicy/common-um/vendor
+
+#SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+#    device/nexus/sepolicy/common-um/private
+
+#SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+#    device/nexus/sepolicy/common-um/public
