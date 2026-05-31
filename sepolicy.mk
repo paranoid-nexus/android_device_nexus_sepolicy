395ec3cc528c2a46420bfa5b28c3297e12c79b83
CuriousNom <aryanbiswas7585@gmail.com>
2026-05-31 20:26:11 +0530

    sepolicy: Label nexus ota packages
    Change-Id: I5060033f0456b0c80cd4a4e91a5784fa903120b0

--- sepolicy.mk
+++ sepolicy.mk
@@ -5,5 +5,5 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/nexus/sepolicy/common/private
 
-#SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-#    device/nexus/sepolicy/common/public
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    device/nexus/sepolicy/common/public
