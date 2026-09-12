1b60f88ea3fbcdf99138a1492db71818a621a0ab
CuriousNom <aryanbiswas7585@gmail.com>
2026-09-12 14:17:36 +0530

    Revert "sepolicy: Move to legacy-um path"
    This reverts commit 5fbbb239766942cafc158b0e9a5b1dfd1bc6544e.

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,6 +1,7 @@
 # Additional Nexus Sepolicy
+
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/nexus/sepolicy/legacy-um/vendor
+    device/nexus/sepolicy/vendor
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/nexus/sepolicy/legacy-um/private
+    device/nexus/sepolicy/private
